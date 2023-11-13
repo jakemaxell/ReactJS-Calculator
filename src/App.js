3d36a2561c354cbca83ac6111f298eb0752e1029
Jacob Maxwell <jmaxwell@tocafootball.com>
2023-11-12 23:44:25 -0500

    Beginning stages of my react calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import * as math from 'mathjs';
 
 function App() {
+  const [value, setValue] = useState("");
+
+  function Button({ buttonSymbol }) {
+
+    function handleClick(){
+      switch(buttonSymbol) {
+        case "AC":
+          setValue("");
+          break;
+        case "+/-":
+          if(value.charAt(0) !== "-"){setValue("-" + value)};
+          break;
+        case "%":
+          if(!value.includes(buttonSymbol)){setValue(value + buttonSymbol)};
+          break;
+        case 0:
+          if(value !== ""){setValue(value + buttonSymbol.toString())};
+          break;
+        case ".":
+          if(!value.includes(buttonSymbol)){setValue(value + buttonSymbol)};
+          break;  
+        case "=":
+          try{
+            const evaluateResults = math.evaluate(value.toString());
+            setValue(evaluateResults);  
+          } catch (error){
+            setValue("Invalid Input.");
+          }
+          break;
+        default:
+          setValue(value + buttonSymbol.toString());
+      }
+    }
+
+    const buttonClassName = buttonSymbol === 0 ? "button special-button" : "button";
+    let shownButtonSymbol = buttonSymbol;
+    if(shownButtonSymbol === "*"){
+      shownButtonSymbol = "X";
+    }
+    else if(shownButtonSymbol === "/"){
+      shownButtonSymbol = "\u00F7";
+    }
+  
+    return <button className={buttonClassName} onClick={handleClick}>{shownButtonSymbol}</button>
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <textarea readOnly={true} className='screen' value={value}/>
+
+      <div className='buttons'>
+        <div className='buttonRow'>
+          <Button buttonSymbol={"AC"}/>
+          <Button buttonSymbol={"+/-"}/>
+          <Button buttonSymbol={"%"}/>
+          <Button buttonSymbol={"/"}/>
+        </div>
+        <div className='buttonRow'>
+          <Button buttonSymbol={7}/>
+          <Button buttonSymbol={8}/>
+          <Button buttonSymbol={9}/>
+          <Button buttonSymbol={"*"}/>
+        </div>
+        <div className='buttonRow'>
+        <Button buttonSymbol={4}/>
+        <Button buttonSymbol={5}/>
+        <Button buttonSymbol={6}/>
+        <Button buttonSymbol={"-"}/>
+        </div>
+        <div className='buttonRow'>
+        <Button buttonSymbol={1}/>
+        <Button buttonSymbol={2}/>
+        <Button buttonSymbol={3}/>
+        <Button buttonSymbol={"+"}/>
+        </div>
+        <div className='buttonRow'>
+          <Button buttonSymbol={0}/>
+          <Button buttonSymbol={"."}/>
+          <Button buttonSymbol={"="}/>
+        </div>
+      
+      </div>
     </div>
   );
 }
